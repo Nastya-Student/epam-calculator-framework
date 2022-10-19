2b9ebf79ec79d4c8ad3a34ae0c8716c9f3b983d6
Nastya89345 <82467736+Nastya89345@users.noreply.github.com>
2022-10-19 12:18:13 +0300

    Update AbstractPage.java

--- selenium-calculator/src/test/java/org/example/page/AbstractPage.java
+++ selenium-calculator/src/test/java/org/example/page/AbstractPage.java
@@ -1,10 +1,13 @@
 package org.example.page;
 
 import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public abstract class AbstractPage {
     protected WebDriver driver;
+     protected static Logger logger = LogManager.getRootLogger();
 
     protected AbstractPage(WebDriver driver) {
         this.driver = driver;

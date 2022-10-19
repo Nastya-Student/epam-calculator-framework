e72a7031ae8c332bd5962302051608e947a6bcfd
Nastya89345 <82467736+Nastya89345@users.noreply.github.com>
2022-10-19 12:26:20 +0300

    Update CloudGooglePage.java

--- selenium-calculator/src/test/java/org/example/page/CloudGooglePage.java
+++ selenium-calculator/src/test/java/org/example/page/CloudGooglePage.java
@@ -6,15 +6,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CloudGooglePage extends AbstractPage{
 
     private final String BASE_URL = "https://cloud.google.com";
     private Instances testInstances;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     @Override
     public CloudGooglePage openPage() {
         driver.navigate().to(BASE_URL);
-        logger.info("ClouGooglePage opened");
+        logger.info("CloudGooglePage opened");
         return this;
     }
 
@@ -28,6 +32,7 @@ public class CloudGooglePage extends AbstractPage{
     private WebElement searchField;
 
     public CloudGoogleResultsPage searchCalculator(String text){
+         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@aria-autocomplete='list']")));
         searchField.click();
         logger.info("searchField clicked");
         searchField.sendKeys(text);
